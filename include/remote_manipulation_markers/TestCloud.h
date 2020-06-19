f04454562ef0a5cc384c8a8b1398965c8e3356a0
David Kent <dekent90@gmail.com>
2020-06-19 17:56:29 -0400

    random point cloud publisher for debugging

---
+++ include/remote_manipulation_markers/TestCloud.h
@@ -0,0 +1,44 @@
+#ifndef TEST_CLOUD_H_
+#define TEST_CLOUD_H_
+
+//ROS
+#include <ros/ros.h>
+#include <remote_manipulation_markers/GenerateGrasps.h>
+#include <boost/thread/mutex.hpp>
+#include <interactive_markers/interactive_marker_server.h>
+#include <pcl_ros/point_cloud.h>
+#include <remote_manipulation_markers/point_cloud_manipulation.h>
+#include <sensor_msgs/PointCloud2.h>
+#include <sensor_msgs/point_cloud_conversion.h>
+#include <tf/transform_listener.h>
+
+//PCL
+#include <pcl/common/common.h>
+#include <pcl/filters/crop_box.h>
+#include <pcl/kdtree/kdtree_flann.h>
+#include <pcl/io/pcd_io.h>
+#include <pcl/point_types.h>
+#include <pcl/point_cloud.h>
+#include <pcl/surface/mls.h>
+
+class TestCloud
+{
+
+public:
+
+    TestCloud();
+
+  void publishCloud();
+
+private:
+  ros::NodeHandle n;
+  ros::NodeHandle pnh;
+
+  //topics
+  ros::Publisher cloudPublisher;
+
+  pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloudPtr;
+  sensor_msgs::PointCloud2 cloud;
+};
+
+#endif  // TEST_CLOUD_H_
---
+++ src/TestCloud.cpp
@@ -0,0 +1,51 @@
+#include <remote_manipulation_markers/TestCloud.h>
+
+using namespace std;
+
+TestCloud::TestCloud() :
+    pnh("~"), cloudPtr(new pcl::PointCloud<pcl::PointXYZRGB>)
+{
+
+  cloudPublisher = n.advertise< pcl::PointCloud<pcl::PointXYZRGB> >("test_cloud", 1);
+
+  // create point cloud object
+  pcl::PointCloud<pcl::PointXYZRGB> myCloud;
+
+  // fill cloud with random points
+  for (int v=0; v<1000; ++v)
+  {
+    pcl::PointXYZRGB newPoint;
+    newPoint.x = (rand() * 0.4) / RAND_MAX;
+    newPoint.y = (rand() * 0.4) / RAND_MAX;
+    newPoint.z = (sin(v/20.0));
+    newPoint.r = 1.0;
+    newPoint.g = 1.0;
+    newPoint.b = 1.0;
+    myCloud.points.push_back(newPoint);
+  }
+
+  *cloudPtr = myCloud;
+
+  ROS_INFO("Point cloud initialized.");
+}
+
+void TestCloud::publishCloud()
+{
+  cloudPublisher.publish(cloudPtr);
+}
+
+
+int main(int argc, char **argv)
+{
+  ros::init(argc, argv, "test_cloud");
+
+  TestCloud tc;
+
+  ros::Rate loop_rate(5);
+  while (ros::ok())
+  {
+    tc.publishCloud();
+    ros::spinOnce();
+    loop_rate.sleep();
+  }
+}
